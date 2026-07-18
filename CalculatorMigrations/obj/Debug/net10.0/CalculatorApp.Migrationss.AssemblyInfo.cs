86915bc0833659a19640835a4756e19be9efee4b
fikayomi123 <folahan08@gmail.com>
2026-07-18 11:21:28 -0700

    docker added

--- CalculatorMigrations/obj/Debug/net10.0/CalculatorApp.Migrationss.AssemblyInfo.cs
+++ CalculatorMigrations/obj/Debug/net10.0/CalculatorApp.Migrationss.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp.Migrationss")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90fabe2d0683ce508689c3ba1f03b0966ea55c45")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp.Migrationss")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp.Migrationss")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
